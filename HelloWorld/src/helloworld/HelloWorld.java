af0ce68e3691214ba9ab6d469cff5ed021f1dfe9
federicodegan <federico.degan@casasoft.ch>
2019-03-11 13:07:57 +0100

    nüt

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("HelloWorld!");
-		System.out.println("test") ;
+		System.out.println("test" ;
 		
 	}
 
