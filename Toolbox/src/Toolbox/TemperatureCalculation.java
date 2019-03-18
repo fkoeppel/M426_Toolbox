03979cba22f928d410e0f21df916b407eade7f61
phzd <phazedclient@gmail.com>
2019-03-18 16:54:41 +0100

    TemperatureCalculation #007 final add

---
+++ Toolbox/src/Toolbox/TemperatureCalculation.java
@@ -0,0 +1,79 @@
+package Toolbox;
+
+public class TemperatureCalculation {
+
+	package Toolbox;
+
+	/*
+	 * author:		Federico Köppel
+	 * element:		007
+	 * date:		18.03.19
+	 * 
+	 */
+
+	import java.text.DecimalFormat;
+
+	public class TemperatureCalculation {
+
+
+		public double CelsiusToFahrenheit(double celsius) {
+
+			double fahrenheit = ((9.0/5.0) * celsius) + 32;
+
+			return decimalFormat(fahrenheit);
+		}
+
+		public double FahrenheitToCelsius(double fahrenheit) {
+
+			double celsius = (5.0/9.0) * (fahrenheit - 32);
+
+			return decimalFormat(celsius);
+		}
+
+
+		public double CelsiusToKelvin(double celsius) {
+
+			double kelvin = celsius + 273.15;
+
+			return decimalFormat(kelvin);
+		}
+
+
+
+		public double FahrenheitToKelvin(double fahrenheit) {
+
+			double kelvin = (fahrenheit + 459.67) * 5/9;
+
+			return decimalFormat(kelvin);
+		}
+
+
+		public double KelvinToCelsius(double kelvin) {
+
+			double celsius = kelvin - 273.15;
+
+			return decimalFormat(celsius);
+		}
+
+
+
+		public double KelvinToFahrenheit(double kelvin) {
+
+			double fahrenheit = (9.0/5.0) * (kelvin - 273.15) + 32;
+
+			return decimalFormat(fahrenheit);
+		}
+
+
+		public double decimalFormat(double result) {
+			DecimalFormat df = new DecimalFormat("###.##");
+			String resultFormatted = df.format(result);
+
+			return Double.parseDouble(resultFormatted);
+		}
+
+
+
+	}
+	
+}
